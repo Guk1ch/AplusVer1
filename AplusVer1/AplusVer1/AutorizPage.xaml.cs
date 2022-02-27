eb4c0f57ea99bce10a943edf9a2bf250eaa1e918
Егор Артемьев <batman_batman_1910@mail.ru>
2022-02-27 20:19:34 +0300

    Add project files.

---
+++ AplusVer1/AplusVer1/AutorizPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AplusVer1
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class AutorizPage : ContentPage
+	{
+		public AutorizPage()
+		{
+			InitializeComponent();
+		}
+
+		private void btn_Registr_Clicked(object sender, EventArgs e)
+		{
+
+		}
+
+		private void btn_Entery_Clicked(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
