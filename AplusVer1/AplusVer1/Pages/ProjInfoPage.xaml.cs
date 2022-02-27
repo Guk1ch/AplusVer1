606544a35bcade9fea4adec254e1c97da9f2ee14
Егор Артемьев <batman_batman_1910@mail.ru>
2022-02-27 20:41:45 +0300

    ъеъ

---
+++ AplusVer1/AplusVer1/Pages/ProjInfoPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AplusVer1.Pages
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ProjInfoPage : ContentPage
+	{
+		public ProjInfoPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
